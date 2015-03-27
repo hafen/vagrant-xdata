ffc907b430caba701aab2ec47d93e60adc9891d9
hafen <rhafen@gmail.com>
2015-03-26 17:53:54 -0700

    Initial Tessera components commit

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # vagrant-xdata
+
 DARPA XData Vagrant VM
 
-Please make sure that vbguest is installed (https://github.com/dotless-de/vagrant-vbguest/).  
+Please make sure that vbguest is installed (https://github.com/dotless-de/vagrant-vbguest/).
+
+```
+vagrant plugin install vagrant-vbguest
+```
+
 It helps keep the VirtuaalBoxGuestAdditions up to date.
+
+Once you `vagrant up`, you can install whatever R packages you need.  Then you need to create an archive of R and all the packages to be sent to all the nodes on the cluster.  You can do this with the following:
+
+```s
+setwd("~/")
+hdfs.setwd("/user/__username__")
+bashRhipeArchive("rhipe")
+```
+
+After it completes, it will tell you what commands to use to initialize RHIPE.
--- deploy.sh
+++ deploy.sh
@@ -1,86 +1,35 @@
 #!/usr/bin/env bash
 
-# Get the Extra repos installed
-sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-sudo rpm -Uvh http://archive.cloudera.com/cdh5/one-click-install/redhat/6/x86_64/cloudera-cdh-5-0.x86_64.rpm
+# R
+sudo yum -y install R
 
-# Get some basics going
-sudo yum -y install wget
-sudo yum -y install docker-io
-sudo yum -y install java-1.7.0-openjdk-devel
-sudo yum -y install hadoop-client
-sudo yum -y install bind-utils
-sudo yum -y install vpnc
-sudo yum -y groupinstall 'Development Tools'
-sudo yum -y install nscd
+sudo R CMD javareconf
+sudo su - -c "R -e \"install.packages('rJava', repos='http://www.rforge.net/')\""
+sudo su - -c "R -e \"install.packages('devtools', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
 
-# Do an update
-sudo yum -y update
+export PROTO_BUF_VERSION=2.5.0
+wget https://protobuf.googlecode.com/files/protobuf-$PROTO_BUF_VERSION.tar.bz2
+tar jxvf protobuf-$PROTO_BUF_VERSION.tar.bz2
+cd protobuf-$PROTO_BUF_VERSION
+./configure && make -j4
+sudo make install
+cd ..
 
-# Spark
-sudo yum -y install spark-core
-sudo yum -y install spark-python
+export RHIPE_VERSION=0.75.1.2_hadoop-2
 
-# HBase
-sudo yum -y install hbase
+wget http://ml.stat.purdue.edu/rhipebin/Rhipe_$RHIPE_VERSION.tar.gz
 
-# Hive
-sudo yum -y install hive
-sudo yum -y install hive-jdbc
-sudo yum -y install hive-hbase
-sudo yum -y install hive-hcatalog
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+sudo chmod 777 /usr/lib64/R/library
+sudo chmod -R 777 /usr/share/
+R CMD INSTALL Rhipe_$RHIPE_VERSION.tar.gz
 
-# Impala
-sudo yum -y install impala
-sudo yum -y install impala-shell
-
-# Pig
-sudo yum -y install pig
-
-# Zookeeper client
-sudo yum -y install zookeeper
-
-# Sqoop
-sudo yum -y install sqoop
-
-# Setup software DIR
-sudo mkdir -p /srv/software
-
-# Solr, aka Cloudera Search
-sudo yum -y install solr
-sudo yum -y install solr-mapreduce
-sudo yum -y install solr-crunch
-sudo ln -s /etc/default/solr /etc/solr/default
-
-# Storm
-sudo wget https://github.com/apache/storm/archive/v0.9.3.tar.gz
-sudo /bin/tar xzvf v0.9.3.tar.gz -C /srv/software
-sudo ln -s /srv/software/storm-0.9.3 /srv/software/storm
-sudo mkdir -p /var/log/storm
-sudo rm -rf /srv/software/storm/conf
-sudo ln -s /var/log/storm /srv/software/storm/logs
-sudo ln -s /etc/storm/conf /srv/software/storm/conf
-sudo mkdir -p /tmp/storm/local
-
-# Get Scala 2.11.5
-sudo wget http://www.scala-lang.org/files/archive/scala-2.11.5.tgz
-sudo /bin/tar xzvf scala-2.11.5.tgz -C /usr/lib
-sudo ln -s /usr/lib/scala-2.11.5 /usr/lib/scala
-sudo echo "export PATH=\$PATH:/usr/lib/scala/bin\nexport SCALA_HOME=/usr/lib/scala" >> /etc/profile.d/scala.sh
-
-# Get MiniConda
-sudo wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh
-sudo /bin/chmod 755 ./Miniconda-3.7.0-Linux-x86_64.sh
-sudo ./Miniconda-3.7.0-Linux-x86_64.sh -b -p /srv/software/anaconda
-sudo echo "export PATH=/srv/software/anaconda/bin:\$PATH" >> /etc/profile.d/anaconda.sh
-sudo /srv/software/anaconda/bin/conda update --yes conda
-sudo /srv/software/anaconda/bin/conda install --yes ipython
-sudo /srv/software/anaconda/bin/conda install --yes ipython-notebook
-
-# create hadoop config files
-HDFS_COMPUTE_CONF=/etc/hadoop/conf.memex
-alternatives --install /etc/hadoop/conf hadoop-conf $HDFS_COMPUTE_CONF 60
-
-# Add the Hadoop Lzo
-sudo wget -P /etc/yum.repos.d http://archive-primary.cloudera.com/gplextras5/redhat/6/x86_64/gplextras/cloudera-gplextras5.repo
-sudo yum -y install hadoop-lzo
+echo export 'LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' | tee -a /home/vagrant/.bashrc
+echo export 'PKG_CONFIG_PATH=/usr/local/lib/pkgconfig' | tee -a /home/vagrant/.bashrc
+echo export 'JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.75.x86_64/jre' | tee -a ~/.bashrc
+echo export 'HADOOP_CONF_DIR=/vagrant/hadoop' | tee -a /home/vagrant/.bashrc
+echo export 'HADOOP_HOME=/usr/lib/hadoop' | tee -a /home/vagrant/.bashrc
+echo export 'HADOOP_BIN=/usr/bin/hadoop:/usr/lib/hadoop/lib' | tee -a /home/vagrant/.bashrc
+echo export "HADOOP_LIBS=`hadoop classpath | tr -d '*'`" | tee -a /home/vagrant/.bashrc
